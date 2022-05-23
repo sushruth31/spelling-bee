331091fad79cb1779be3123c47b507ddb2792297
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-05-22 22:44:30 -0400

    updates

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import Hexagon from "react-hexagon"
+import { useEffect, useRef, useState } from "react"
+import useVis from "./useVis"
 const NUM_LETTERS = 7
 
 function shuffle(arr) {
@@ -52,20 +52,111 @@ function createLettersOfTheDay() {
   }
 }
 
+let createStyle = (top, left, style) => ({
+  top,
+  left,
+  ...style,
+})
 export default function Main() {
   let [lettersOfTheDay, setLettersOfTheDay] = useState(createLettersOfTheDay)
   let [attempts, setAttempts] = useState([])
+  let hTop = "445px"
+  let h1 = "495px"
+  let h2 = "600px"
+  let hBottom = "655px"
+  let hCenter = "550px"
+  let wLeft = "100px"
+  let wMid = "200px",
+    wRight = "300px"
+
+  let positions = [
+    [hTop, wMid],
+    [h1, wRight],
+    [h2, wRight],
+    [hBottom, wMid],
+    [h2, wLeft],
+    [h1, wLeft],
+  ]
+
+  let hexStyles = positions.map(p => createStyle(...p))
+  let centerStyle = createStyle(hCenter, wMid, {})
+  let [inputText, setInputText] = useState([])
+  let caretVis = useVis()
+
+  let deleteLetter = () => {
+    caretVis.show()
+    setInputText(p => p.slice(0, p.length - 1))
+  }
+  let addLetter = ltr => {
+    caretVis.show()
+    setInputText(p => [...p, ltr])
+  }
+
+  let handleKey = e => {
+    console.log(e.key)
+    if (e.key === "Control" || e.key === "Shift" || e.key === "Meta") return
+    let key = e.key.toUpperCase()
+    if (key === "BACKSPACE" || key === "DELETE") {
+      return deleteLetter()
+    }
+    addLetter(key)
+  }
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKey)
+    return () => window.removeEventListener("keyup", handleKey)
+  }, [])
+
+  let textColor = ltr => {
+    if (ltr === lettersOfTheDay.centerLetter) {
+      return "#f7da21"
+    }
+    if (lettersOfTheDay.letters.includes(ltr)) {
+      return "black"
+    }
+    return "#e6e6e6"
+  }
+
+  useEffect(() => {
+    let id = setInterval(caretVis.toggle, 500)
+    return () => clearInterval(id)
+  }, [])
 
   return (
-    <>
-      <div style={{ top: 200 }}>
-        <div class="hexagon hex1"></div>
-        <div class="hexagon hex2"></div>
-        <div class="hexagon hex3"></div>
-        <div class="hexagon hex4"></div>
-        <div class="hexagon hex5"></div>
-        <div class="hexagon hex6"></div>
+    <div className="flex flex-col items-center justify-center p-20">
+      <div className="w-80 h-10 flex items-center px-4">
+        {inputText.map(ltr => (
+          <div
+            style={{ color: textColor(ltr) }}
+            className={`font-bold text-3xl`}
+          >
+            {ltr}
+          </div>
+        ))}
+        {caretVis.visible && <div className="h-full w-1 bg-[#f7da21]"></div>}
       </div>
-    </>
+      <div style={{ top: 200 }}>
+        {lettersOfTheDay.letters.map((ltr, i) => {
+          return (
+            <button
+              onClick={() => addLetter(ltr)}
+              style={hexStyles[i]}
+              className="hexagon flex items-center justify-center"
+            >
+              <div className="font-bold text-3xl">{ltr}</div>
+            </button>
+          )
+        })}
+        <button
+          onClick={() => addLetter(lettersOfTheDay.centerLetter)}
+          className="hexagonCenter flex items-center justify-center "
+          style={centerStyle}
+        >
+          <div className="font-bold text-3xl">
+            {lettersOfTheDay.centerLetter}
+          </div>
+        </button>
+      </div>
+    </div>
   )
 }
--- src/modal.js
+++ src/modal.js
@@ -11,7 +11,10 @@ export default function Modal() {
         className="fixed top-0 left-0 h-screen w-full flex items-center justify-center"
         style={{ background: "rgba(0,0,0,0.8)" }}
       >
-        <div className="bg-white relative p-5 shadow-lg rounded flex flex-col items-start text-lg text-gray-800">
+        <div
+          style={{ zIndex: 5000 }}
+          className="bg-white relative p-5 shadow-lg rounded flex flex-col items-start text-lg text-gray-800"
+        >
           {content}
         </div>
       </div>,
